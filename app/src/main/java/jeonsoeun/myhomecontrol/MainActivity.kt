3bba425934b3360cfedaaeb2b58ebbc07699b979
Jun <jse525@naver.com>
2020-09-28 14:32:11 +0900

    메세지띄워봄, 합성 프로퍼티 사용해봄

--- app/src/main/java/jeonsoeun/myhomecontrol/MainActivity.kt
+++ app/src/main/java/jeonsoeun/myhomecontrol/MainActivity.kt
@@ -2,10 +2,19 @@ package jeonsoeun.myhomecontrol
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        //val mainText = findViewById<TextView>(R.id.main_text)
+       init()
+    }
+    fun MainActivity.init() {
+        // 합성 프로퍼티 사용. 이게 findViewById를 대신해준다.
+        main_text.text = "오 이렇게 한번에 되네."
+        main_text.visibility = View.VISIBLE
     }
 }
